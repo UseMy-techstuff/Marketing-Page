b9b5eaf391f3859bd2d1f3e0a7646ab2663422ef
sid2015980 <sidneybroadnax@gmail.com>
2020-03-05 11:11:25 -0700

    finished

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
-const slide = document.querySelector(".bgslide");
+// const slide = document.querySelector(".bgslide");  This had bug issues so I changed it
 
-let count = 1;
+// let count = 1;
 
-function changeImg(){
-    console.log(count)
-    slide.style.backgroundImage = `url(/img/img${count}.jpg)`
-setTimeout(() => {
-    count++
-}, 1000);
-    if (count === 5){
-        count = 0;
-    }
+// function changeImg(){
+//     console.log(count)
+//     slide.style.backgroundImage = `url(/img/img${count}.jpg)`
+// setTimeout(() => {
+//     count++
+// }, 1000);
+//     if (count === 5){
+//         count = 0;
+//     }
+// }
+
+// setInterval(
+//  changeImg, 3000);
+
+const slideImg = document.querySelectorAll(".bgslide .slide-img");
+
+const nextImageDelay = 4000;
+
+let currentImageCounter = 0;
+
+slideImg[currentImageCounter].style.opacity = 1;
+
+setInterval(nextImage, nextImageDelay);
+
+function nextImage() {
+    const tempCounter = currentImageCounter;
+    setTimeout(() => {
+        slideImg[tempCounter].style.opacity = 0;
+    }, 1000);
+    currentImageCounter = (currentImageCounter + 1) % slideImg.length;
+    slideImg[currentImageCounter].style.opacity = 1;
 }
-
-setInterval(
- changeImg, 3000);
